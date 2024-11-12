09fd2a87ef9aa31d8265ea7ae0f14419683bbe6f
Daniele Muffato <daniele.muffato@studio.unibo.it>
2024-11-12 09:09:08 +0100

    method to add connections

--- distance_vector_routing.py
+++ distance_vector_routing.py
@@ -3,12 +3,13 @@
 class router():
     nicks = []
     ipaddr = []
+    connections = {}
 
     def __init__(self, name):
         self.name = name
 
     def __str__(self):
-        return f'{self.nicks}, {self.ipaddr}'
+        return f'{self.nicks}, {self.ipaddr}, {self.connections}'
 
     def addNick(self, interfaceName, ipAddr):
         """
@@ -17,3 +18,21 @@ class router():
         """
         self.nicks.append(interfaceName)
         self.ipaddr.append(ipAddr)
+
+    def addConnection(self, ipAddrSelf, ipAddrOther):
+        if ipAddrSelf not in self.ipaddr:
+            raise ValueError("invalid connection!")
+        if ipAddrSelf not in self.connections:
+            self.connections[ipAddrSelf] = [ipAddrOther]
+        else:
+            conn = self.connections[ipAddrSelf]
+            if ipAddrOther in conn:
+                raise ValueError("connecton already established")
+            conn.append(ipAddrOther)
+
+
+routerA = router("A")
+routerA.addNick("eth1", "192.168.1.1")
+routerA.addConnection("192.168.1.1", "192.168.1.2")
+routerA.addConnection("192.168.1.1", "192.168.1.3")
+print(routerA)
