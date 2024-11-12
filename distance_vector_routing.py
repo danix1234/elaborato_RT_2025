3211045f35f03e33d574025e6c768a431b66eb7f
Daniele Muffato <daniele.muffato@studio.unibo.it>
2024-11-12 08:16:22 +0100

    updated files

--- distance_vector_routing.py
+++ distance_vector_routing.py
@@ -1,15 +1,5 @@
 #!/bin/python3
 
-def updateRoutingTable(oldRib, dest, destMask, nextHop, distance):
-    oldRib.append((dest, destMask, nextHop, distance))
-    return oldRib
-
-
-def printRoutingTable(rib):
-    for elem in rib:
-        destination = str(elem[0]) + "/" + str(elem[1])
-        print(destination, "via", elem[2], "[distance:", str(elem[3])+"]")
-
-
-rib = updateRoutingTable([], "192.168.4.0", "24", "192.168.1.2", 2)
-printRoutingTable(rib)
+class router():
+    def __init__(self, name):
+        self.name = name
