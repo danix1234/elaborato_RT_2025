de5426ff7a970338968d2fb79dcd80ed745c24cb
Daniele Muffato <daniele.muffato@studio.unibo.it>
2024-11-12 08:58:27 +0100

    added methods for router class

--- distance_vector_routing.py
+++ distance_vector_routing.py
@@ -1,8 +1,19 @@
 #!/bin/python3
 
 class router():
+    nicks = []
+    ipaddr = []
+
     def __init__(self, name):
         self.name = name
 
     def __str__(self):
-        return 'todo'
+        return f'{self.nicks}, {self.ipaddr}'
+
+    def addNick(self, interfaceName, ipAddr):
+        """
+        interfaceName and ipAddr must be relative to the same nick
+        both interfaceName and ipAddr must be strings
+        """
+        self.nicks.append(interfaceName)
+        self.ipaddr.append(ipAddr)
